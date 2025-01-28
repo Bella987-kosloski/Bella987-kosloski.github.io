f6e328919ff9119e354557df5df6bd2fb8ffc60e
Bella987-kosloski <biakosloski@gmail.com>
2025-01-28 17:06:12 -0300

    vaiir

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-
 export default {
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
-    }
+  kit: {
+      adapter: adapter({
+          pages: 'build',
+          assets: 'build',
+          fallback: undefined,
+          precompress: false,
+          strict: true
+      })
+  }
 };
